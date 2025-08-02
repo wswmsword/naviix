834aa1e158bc0d1e0e2bb5a6ee279f0b85c727d3
wswmsword <cjzjzzz@gmail.com>
2025-08-02 18:27:02 +0800

    update example

--- example/switch2/src/components/kit/scroll-view.tsx
+++ example/switch2/src/components/kit/scroll-view.tsx
@@ -13,44 +13,167 @@ export default function ScrollView() {
     x.current = naviix(gamesE.current);
   }, []);
 
-  return <div id="gms" className="border w-full max-w-7xl h-30 overflow-x-auto flex gap-3 items-center px-3" ref={wrapE} onKeyDown={keyNav}>
-    {games.map((g, i) => <button className="w-20 h-20 border shrink-0 rounded-sm" key={i} ref={e => { if(e) gamesE.current[i] = e }}>1</button>)}
+  let isKeyPressed = false;
+  let isKeyLongPressed = false;
+  const longPressTime = 300;
+  const scrollSpeed = 160;
+  const dynamicSpeed = useRef(-1);
+  const focusInterval = 84; // 1000 / 60 * 5
+  const safeWidth = 70; // 矩形的最右侧要在安全宽度左侧，矩形的最左侧要在安全宽度右侧
+  /** 按下方向键后首次聚焦 */
+  const focusedFirst = useRef(false);
+  const moveRRafId = useRef<number>(-1);
+  const safeScrollRafId = useRef<number>(-1);
+  const spf = useRef<number>(-1);
+
+  useEffect(() => {
+    detectRefreshRate(rate => {
+      spf.current = 1000 / rate;
+      dynamicSpeed.current = Math.round(scrollSpeed * 1000 / rate / focusInterval); // scrollSpeed / Math.round(focusInterval / spf.current)
+      // console.log(Math.round(focusInterval / spf.current), spf.current, dynamicSpeed.current)
+    });
+    function detectRefreshRate(callback: (rate: number) => void, sampleCount = 60) {
+      let times: number[] = [];
+      let last = performance.now();
+
+      function frame(now: number) {
+        const delta = now - last;
+        last = now;
+
+        times.push(delta);
+
+        if (times.length < sampleCount) {
+          requestAnimationFrame(frame);
+        } else {
+          // 去掉第一个不准的帧差
+          times = times.slice(1);
+          // 平均帧间隔（毫秒）
+          const avg = times.reduce((a, b) => a + b, 0) / times.length;
+          // 刷新率 = 每秒帧数
+          const refreshRate = Math.round(1000 / avg);
+          callback(refreshRate);
+        }
+      }
+
+      requestAnimationFrame(frame);
+    }
+  }, []);
+
+  return <div id="gms" className="border w-full max-w-7xl h-70 overflow-x-auto flex gap-10 items-center px-10" ref={wrapE}
+    onKeyDown={keyNav}
+    onKeyUp={keyUp}>
+    {games.map((g, i) => <button className="w-50 h-50 border shrink-0 rounded-sm" key={i} ref={e => { if(e) gamesE.current[i] = e }}>1</button>)}
   </div>;
 
+  function keyUp(e: KeyboardEvent) {
+    if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
+      isKeyPressed = false;
+      isKeyLongPressed = false;
+      cancelAnimationFrame(moveRRafId.current);
+    }
+  }
+
   function keyNav(e: KeyboardEvent) {
-    const nextInfo = x.current.get(e.target);
-    if (e.key === "ArrowRight") {
+    if (e.key === "ArrowRight") e.preventDefault();
+    if (e.key === "ArrowRight" && !isKeyPressed) {
+      isKeyPressed = true;
+      isKeyLongPressed = false;
+      focusedFirst.current = false;
       e.preventDefault();
-      if (nextInfo.right) {
-        const nextE = nextInfo.right.id;
-        const _wrapE = wrapE.current as HTMLDivElement;
-        const isInView = isElementInHorizontalView(nextE, _wrapE);
-        if (!isInView) {
-          // gap 12px, w/3 30px
-          const elRight = nextE.offsetLeft + nextE.offsetWidth;
-          const wrapRight = _wrapE.scrollLeft + _wrapE.clientWidth;
-          const extraW = elRight - wrapRight;
-          _wrapE.scrollTo({ left: _wrapE.scrollLeft + extraW + 42, behavior: "smooth" });
+      cancelAnimationFrame(safeScrollRafId.current);
+      moveRRafId.current = requestAnimationFrame(moveR)
+
+      // 聚焦
+      // 控制滚动，滚动到安全区域
+      let lastFocusTime = -Infinity;
+
+      function moveR() {
+        const cur = Date.now();
+        const nextInfo = x.current.get(document.activeElement);
+        if (nextInfo.right) {
+          const nextE = nextInfo.right.id;
+          const _wrapE = wrapE.current as HTMLDivElement;
+
+          const isOutOfView = isElementOutOfHorizontalView(nextE, _wrapE);
+
+          if (isOutOfView) {
+            _wrapE.scrollLeft += dynamicSpeed.current;
+            moveRRafId.current = requestAnimationFrame(moveR);
+            return ;
+          }
+
+          if (!focusedFirst.current) {
+            focusedFirst.current = true;
+            lastFocusTime = cur;
+            nextE.focus({ preventScroll: true });
+            scrollToSafeArea();
+            return ;
+          } else if (!isKeyLongPressed) {
+            if (cur - lastFocusTime > longPressTime) {
+              isKeyLongPressed = true;
+              lastFocusTime = cur;
+              nextE.focus({ preventScroll: true });
+              scrollToSafeArea();
+            } else moveRRafId.current = requestAnimationFrame(moveR);
+          } else {
+            if (cur - lastFocusTime > focusInterval) {
+              lastFocusTime = cur;
+              nextE.focus({ preventScroll: true });
+              scrollToSafeArea();
+            } else moveRRafId.current = requestAnimationFrame(moveR);
+          }
+
+          function scrollToSafeArea() {
+            const elRight = nextE.offsetLeft + nextE.offsetWidth;
+            const wrapRight = _wrapE.scrollLeft + _wrapE.clientWidth;
+            if (wrapRight - elRight > safeWidth) {
+              if (isKeyPressed) moveR();
+              return ;
+            }
+            _wrapE.scrollLeft += dynamicSpeed.current;
+            safeScrollRafId.current = requestAnimationFrame(scrollToSafeArea);
+          }
+          return ;
         }
-        nextE.focus({ preventScroll: true });
       }
-    } else if (e.key === "ArrowLeft") {
+      
+    } else if (e.key === "ArrowLeft" && !isKeyPressed) {
       e.preventDefault();
-      if (nextInfo.left) {
-        const _wrapE = wrapE.current as HTMLDivElement;
-        const nextE = nextInfo.left.id;
-        const isInView = isElementInHorizontalView(nextE, wrapE.current as HTMLDivElement);
-        if (!isInView) {
-          const elRight = nextE.offsetLeft + nextE.offsetWidth;
-          const wrapLeft = _wrapE.scrollLeft;
-          const extraW = nextE.offsetWidth - (elRight - wrapLeft);
-          _wrapE.scrollTo({ left: _wrapE.scrollLeft - extraW - 42, behavior: "smooth" });
+      moveL()
+
+      function moveL() {
+        const nextInfo = x.current.get(document.activeElement);
+        if (nextInfo.left) {
+          const nextE = nextInfo.left.id;
+          nextE.focus();
         }
-        nextE.focus({ preventScroll: true });
       }
     }
   }
 
+  /** 元素部分进入视图 */
+  function isElementPartiallyVisibleInHorizontalView(el: HTMLElement, container: HTMLElement) {
+    const elLeft = el.offsetLeft;
+    const elRight = elLeft + el.offsetWidth;
+    
+    const viewLeft = container.scrollLeft;
+    const viewRight = viewLeft + container.clientWidth;
+
+    return (elLeft < viewRight && elRight > viewRight) || (elLeft < viewLeft && elRight > viewLeft);
+  }
+
+  /** 元素完全没有进入视图 */
+  function isElementOutOfHorizontalView(el: HTMLElement, container: HTMLElement) {
+    const elLeft = el.offsetLeft;
+    const elRight = elLeft + el.offsetWidth;
+    
+    const viewLeft = container.scrollLeft;
+    const viewRight = viewLeft + container.clientWidth;
+
+    return elLeft > viewRight || elRight < viewLeft;
+  }
+
+  /** 元素完全进入视图 */
   function isElementInHorizontalView(el: HTMLElement, container: HTMLElement) {
     const elLeft = el.offsetLeft;
     const elRight = elLeft + el.offsetWidth;
